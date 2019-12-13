f231fd2eefe8cf21ac9624afae155e7b0256e906
lixuefengsjh <1203496509@qq.com>
2019-12-13 18:01:05 +0800

    1.提交mybatieplus冲突导致springboot的ioc异常问题

--- provider-8001/src/main/java/com/lxf/dao/DeptMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lxf.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lxf.common.entity.Dept;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DeptMapper extends BaseMapper<Dept> {
-}
--- provider-8001/src/test/java/com/lxf/test/SampleTest.java
+++ provider-8001/src/test/java/com/lxf/test/SampleTest.java
@@ -1,23 +1,66 @@
 package com.lxf.test;
 
-import com.lxf.dao.DeptMapper;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SampleTest {
-    @Autowired
-    private DeptMapper deptMapper;
-
-
 
     @Test
-    public void testSelect() {
-        System.out.println(deptMapper.selectById(1));
-    }
+    public void testGenerator(){
+        //1、全局配置
+        GlobalConfig config = new GlobalConfig();
+        config.setActiveRecord(true)//开启AR模式
+                .setAuthor("lixuefeng")//设置作者
+                //生成路径(一般都是生成在此项目的src/main/java下面)
+                .setOutputDir("D:\\work\\code\\soa\\provider-8001\\src\\main\\java")
+                .setFileOverride(true)//第二次生成会把第一次生成的覆盖掉
+                .setIdType(IdType.AUTO)//主键策略
+                .setServiceName("%sService")//生成的service接口名字首字母是否为I，这样设置就没有I
+                .setBaseResultMap(true)//生成resultMap
+                .setBaseColumnList(true);//在xml中生成基础列
+        //2、数据源配置
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)//数据库类型
+                .setDriverName("com.mysql.cj.jdbc.Driver")
+                .setUrl("jdbc:mysql://rm-6wel397409ovli572jo.mysql.japan.rds.aliyuncs.com:3306/tcmart_user?useUnicode=true&characterEncoding=utf8&useSSL=false")
+                .setUsername("root")
+                .setPassword("Tcmart321");
+        //3、策略配置
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setCapitalMode(true)//开启全局大写命名
 
+                .setNaming(NamingStrategy.underline_to_camel)//下划线到驼峰的命名方式
+                .setTablePrefix("tb_")//表名前缀
+                .setEntityLombokModel(true)//使用lombok
+                .setInclude("user_new");//逆向工程使用的表
+        //4、包名策略配置
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setParent("com.lxf")//设置包名的parent
+                .setMapper("mapper")
+                .setService("service")
+                .setController("controller")
+            //    .setEntity("entity")
+                .setXml("resources\\mapper");//设置xml文件的目录
+        //5、整合配置
+        AutoGenerator autoGenerator = new AutoGenerator();
+        autoGenerator.setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(packageConfig);
+        //6、执行
+        autoGenerator.execute();
+    }
 }
+
